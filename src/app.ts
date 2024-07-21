07469d7ce79c1f3f1e15875306ca8b5a965e5a6e
Oleg <olegpl1993@gmail.com>
2024-07-21 23:00:46 +0300

    feat: track

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,14 @@ import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders";
 import { Control } from "./engine/Control";
-import { Shapes } from "./shapes/Shapes";
+import { Track } from "./engine/Track";
+import { openInspector } from "./utils/openInspector";
 
 class App {
   private carMesh: AbstractMesh;
   private camera: FollowCamera;
   private control: Control = new Control();
+  private carSpeed: number = 0.1;
 
   constructor() {
     let canvas = document.createElement("canvas");
@@ -27,24 +29,18 @@ class App {
 
     let engine = new Engine(canvas, true);
     let scene = new Scene(engine);
+    const track = new Track(scene);
+    track.createTrack();
 
     window.addEventListener("keydown", this.control.handleKeyDown);
     window.addEventListener("keyup", this.control.handleKeyUp);
 
     this.camera = new FollowCamera("FollowCam", new Vector3(0, 0, 0), scene);
-    this.camera.heightOffset = 10;
-    this.camera.radius = 10;
     this.camera.rotationOffset = 180;
+    this.camera.heightOffset = 6;
+    this.camera.radius = 10;
 
     new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-
-    SceneLoader.ImportMesh("", "models/", "platform.glb", scene, (meshes) => {
-      meshes[0].position = new Vector3(0, 0, 0);
-      for (let i = 1; i <= 100; i++) {
-        let newPlatform = meshes[0].clone(`platform_${i}`, null);
-        if (newPlatform) newPlatform.position = new Vector3(0, 0, i * 5);
-      }
-    });
 
     SceneLoader.ImportMesh("", "models/", "car.glb", scene, (meshes) => {
       this.carMesh = meshes[0];
@@ -52,25 +48,13 @@ class App {
       this.camera.lockedTarget = this.carMesh;
     });
 
-    const shapes = new Shapes(scene);
-    shapes.createCube({ x: 2, y: 0.5, z: 0 });
-
-    // hide/show the Inspector
-    window.addEventListener("keydown", (ev) => {
-      // Shift+Ctrl+Alt+I
-      if (ev.shiftKey && ev.ctrlKey && ev.altKey && ev.keyCode === 73) {
-        if (scene.debugLayer.isVisible()) {
-          scene.debugLayer.hide();
-        } else {
-          scene.debugLayer.show();
-        }
-      }
-    });
+    // hide/show the Inspector Shift+Ctrl+Alt+I
+    window.addEventListener("keydown", (e) => openInspector(e, scene));
 
     engine.runRenderLoop(() => {
       if (!this.carMesh) return;
 
-      this.carMesh.position.z += 0.05;
+      this.carMesh.position.z += this.carSpeed;
 
       if (this.control.moveLeft && this.carMesh.position.x > -3.5) {
         this.carMesh.position.x -= this.control.speed;
