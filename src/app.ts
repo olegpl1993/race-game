6a0053bfab18baee9ce60ad8f26d1337ac5d3490
Oleg <olegpl1993@gmail.com>
2024-07-28 21:46:55 +0300

    feat: class debug inspector

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,25 @@ import { Engine, Scene } from "@babylonjs/core";
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders";
+import { DebugInspector } from "./engine/DebugInspector";
 import { Game } from "./game/Game";
-import { openInspector } from "./utils/openInspector";
 
 class App {
   constructor() {
-    let canvas = document.createElement("canvas");
+    const canvas = document.createElement("canvas");
     canvas.style.width = "100%";
     canvas.style.height = "100%";
     canvas.id = "gameCanvas";
     document.body.appendChild(canvas);
 
-    let engine = new Engine(canvas, true);
-    let scene = new Scene(engine);
+    const engine = new Engine(canvas, true);
+    const scene = new Scene(engine);
 
     const game = new Game();
     game.startGame(engine, scene);
 
-    // hide/show the Inspector Shift+Ctrl+Alt+I
-    window.addEventListener("keydown", (e) => openInspector(e, scene));
+    new DebugInspector(scene);
   }
 }
+
 new App();
--- src/shapes/Shapes.ts
+++ src/shapes/Shapes.ts
@@ -3,16 +3,6 @@ import { Mesh, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
 export class Shapes {
   constructor(private scene: Scene) {}
 
-  createSphere = ({ x, y, z }: { x: number; y: number; z: number }): Mesh => {
-    const sphere: Mesh = MeshBuilder.CreateSphere(
-      "sphere1",
-      { diameter: 1 },
-      this.scene
-    );
-    sphere.position = new Vector3(x, y, z);
-    return sphere;
-  }
-
   createCube = ({ x, y, z }: { x: number; y: number; z: number }): Mesh => {
     const cube: Mesh = MeshBuilder.CreateBox(
       "cube1",
@@ -21,5 +11,5 @@ export class Shapes {
     );
     cube.position = new Vector3(x, y, z);
     return cube;
-  }
+  };
 }
